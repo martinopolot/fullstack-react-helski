a9180508f50865929bed238ffc528dca149015e3
martinopolot <maopolot@outlook.com>
2024-09-14 15:16:57 +0300

    Modified- placing obj into array

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,32 +1,29 @@
-// import Header from "./Header";
-// import Content from "./Content";
+import Header from "./Header";
+import Content from "./Content";
 
 import Total from "./Total";
 function App() {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundementals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
-      {/* <Header />
-      <Content />
-      <Total /> */}
-      <h1>{course}</h1>
-      <p>{part1}</p>
-      <p>{part2}</p>
-      <p>{part3}</p>
-      <Total />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
