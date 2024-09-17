35154811411363f199b87d6c5b3832e03695ce90
martinopolot <maopolot@outlook.com>
2024-09-18 01:10:10 +0300

    unicafe-1.6

---
+++ part1-b-exe-1.6-1.14/README.md
@@ -0,0 +1,4 @@
+# Exercises 1.6.-1.14
+
+- Like most companies, the student restaurant of the University of Helsinki [Unicafe](https://www.unicafe.fi/) collects feedback from its customers.
+- Your task is to implement a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.
---
+++ part1-b-exe-1.6-1.14/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // lets calculate the score averagely (good + neutral + bad) divide by 3
+  const all = good + neutral + bad;
+  console.log(`Total score is ${all}`);
+  const average = all > 0 ? (good - bad) / all : 0;
+  console.log(`Average score is ${average}`);
+
+  // create handlers for buttons clicked
+  const handleClickedGood = () => setGood(good + 1);
+  const handleClickedNeutral = () => setNeutral(neutral + 1);
+  const handleClickedBad = () => setBad(bad + 1);
+
+  console.log("clicked the button");
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleClickedGood}>good</button>
+      <button onClick={handleClickedNeutral}>nuetral</button>
+      <button onClick={handleClickedBad}>bad</button>
+      <h2>Statistics</h2>
+      good: {good}
+      <br />
+      neutral: {neutral}
+      <br />
+      bad: {bad}
+      <br />
+      all {all}
+      <br />
+      average {average}
+    </div>
+  );
+};
+
+export default App;
