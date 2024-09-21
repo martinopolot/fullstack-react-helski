e46a6b8c0da8c3ccf7398eaf321a7c58baa05538
martinopolot <maopolot@outlook.com>
2024-09-21 13:44:00 +0300

    total number of exercises added

--- part2-a/src/App.jsx
+++ part2-a/src/App.jsx
@@ -1,10 +1,58 @@
-const App = (props) => {
-  console.log(props);
+const Course = ({ course }) => {
+  // Show also the sum of the exercises of the course.
+  const numberOfExercises = course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+  console.log(`total of ${numberOfExercises} exercises`);
   return (
     <div>
-      <h1>Hello rendering </h1>
+      <h1>{course.name}</h1>
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+      <strong>total of {numberOfExercises} exercises</strong>
     </div>
   );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
 };
 
 export default App;
